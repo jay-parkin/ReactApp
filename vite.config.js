e05a9f1af9d73893deac871dd4c0c214fca0517d
Jason Parkin <jay-parkin91@hotmail.com>
2024-11-09 11:21:30 +1100

    feat> init react app with example test

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true, // use "describe", "expect", etc etc, without importing them in our test files
+    environment: "jsdom", // makes our tests work better as if they are in the browser
+    setupFiles: "src/setupTest.js", // do any test config before tests run. Runs once before ANY tests, not once before each test file.
+  },
+});
